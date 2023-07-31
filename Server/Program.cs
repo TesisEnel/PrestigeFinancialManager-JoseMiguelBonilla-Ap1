460d17000bd12325ae962eed05817616ac4e5c06
Jose Bonilla <josemiguelbonillamar@gmail.com>
2023-07-30 23:37:10 -0400

    preparando para migracion

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using PrestigeFinancial.Server.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(options => options.UseSqlite(ConStr));
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
