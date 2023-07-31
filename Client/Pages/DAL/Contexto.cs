27daf27a0725ac05fb5bbdd750d2b3ec04e3ccaf
Jose Bonilla <josemiguelbonillamar@gmail.com>
2023-07-30 23:35:12 -0400

    terminando models

---
+++ Client/Pages/DAL/Contexto.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using PrestigeFinancial.Shared.Models;
+
+namespace PrestigeFinancial.Server.DAL 
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+        public DbSet<Clientes> Clientes {get;set;}        
+        public DbSet<ClientesDetalle> ClientesDetalle {get;set;}
+        public DbSet<Prestamos> Prestamos {get;set;}
+        public DbSet<TiposTelefonos> TiposTelefonos {get;set;}
+        public DbSet<TiposPrestamos> TiposPrestamos {get;set;}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TiposTelefonos>().HasData(new List<TiposTelefonos>()
+        {
+            new TiposTelefonos(){TiposTelefonoId=1,  Descripcion="Celular"},
+            new TiposTelefonos(){TiposTelefonoId=2,  Descripcion="Residencial"},
+            new TiposTelefonos(){TiposTelefonoId=3,  Descripcion="Oficina"},
+        });
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TiposPrestamos>().HasData(new List<TiposPrestamos>()
+        {
+            new TiposPrestamos(){TiposPrestamoId=1,  DescripcionPrestamo="Personal"},
+            new TiposPrestamos(){TiposPrestamoId=2,  DescripcionPrestamo="Automotriz"},
+            new TiposPrestamos(){TiposPrestamoId=3,  DescripcionPrestamo="Hipotecario"},
+        });
+        
+        }
+    }
+}
--- Shared/Models/TiposPrestamos.cs
+++ Shared/Models/TiposPrestamos.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PrestigeFinance.Shared.Models
+namespace PrestigeFinancial.Shared.Models
 {
     public class TiposPrestamos
     {
