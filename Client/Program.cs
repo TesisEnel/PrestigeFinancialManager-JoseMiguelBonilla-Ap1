48b6ad22e3a02053c5ba75501c871bad92bc30a1
Jose Bonilla <josemiguelbonillamar@gmail.com>
2023-08-01 00:36:35 -0400

    login

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,10 +2,19 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PrestigeFinancial.Client;
 
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using PrestigeFinancial.Client.Extensiones;
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, AutenticacionExtension>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
---
+++ Server/Controllers/UsuarioController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrestigeFinancial.Shared;
+
+namespace PrestigeFinancial.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            SesionDTO sesionDTO = new SesionDTO();
+
+            if(login.Correo == "admin@gmail.com" && login.Clave == "admin")
+            {
+                sesionDTO.Nombre = "admin";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Administrador";
+            }
+            else
+            {
+                sesionDTO.Nombre = "empleado";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Empleado";
+            }
+
+            return StatusCode(StatusCodes.Status200OK, sesionDTO);
+        }
+
+    }
+}
